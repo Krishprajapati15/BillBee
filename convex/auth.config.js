cdf1c4b04c6725e712bda6a2ec6dba037f1b5fe6
Krishprajapati15 <prajapatikrish132005@gmail.com>
2025-05-11 20:32:32 +0530

    chnge the auth config file

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,12 +1,3 @@
-// export default {
-//   providers: [
-//     {
-//       domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
-//       applicationID: "convex",
-//     },
-//   ],
-// };
-
 import { authConfig } from "convex/server";
 import { clerkAuth } from "convex/server/clerk";
 
